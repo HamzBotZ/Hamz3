b32d274df61125a986285b4cc2e613cb673e1c92
HamzGanz <105532183+HamzBotZ@users.noreply.github.com>
2022-07-29 12:06:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,24 +12,24 @@ global.APIKeys = {
 }
 
 // Other
-global.namabot = ['AzzBot-MD']
-global.namaowner = ['YahyaGanzz']
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.packname = 'AzzBot-MD'
-global.author = 'WhatsApp•Bot'
+global.namabot = ['ZxynnBotz']
+global.namaowner = ['HamzGanzz']
+global.owner = ['6288214989352']
+global.premium = ['6288214989352']
+global.packname = 'ZxynnBotz'
+global.author = 'HamzGanzz'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'DoaMu Terkabulkan',
+    success: 'Done Ngab',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Jadikan Bot Admin Terlebih Dahulu!',
     owner: 'Khusus Owner Bot',
     group: 'Itu Khusus Grup Bre...!',
     private: 'Cuma Bisa Chat Pribadi Bre...',
     bot: 'Khusus Nomor Bot',
-    wait: 'Tunggu Sejam...',
+    wait: 'Orang Sabar Disayang Janda...',
     endLimit: 'Limit Harian Sudah Habis, Limit Akan Direset Besok',
 }
 //Kalo Ngasih Limit Jangan Dikit Lah Bng
